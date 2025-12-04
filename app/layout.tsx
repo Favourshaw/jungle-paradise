272431e1aa85e3c985af2f4b84dbe8214b62d54f
Favour Shaw <favourvanderwaal@gmail.com>
2025-12-04 11:26:16 +0100

    Add Eater font support and update layout to include new font styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,11 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Eater, Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+
+<link
+  href="https://fonts.googleapis.com/css2?family=Eater&family=Geist:wght@100..900&family=Sora:wght@100..800&display=swap"
+  rel="stylesheet"
+/>;
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -10,6 +15,12 @@ const geistSans = Geist({
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
+});
+
+const eater = Eater({
+  subsets: ["latin"],
+  weight: "400",
+  variable: "--font-eater",
 });
 
 export const metadata: Metadata = {
@@ -25,9 +36,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} ${eater.variable} antialiased`}
       >
-        {children}
+        <div className={eater.className}>{children}</div>
       </body>
     </html>
   );
