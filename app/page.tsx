bfeb12b9ebe23d6d43890b5b3b80f8d522597893
Favour Shaw <favourvanderwaal@gmail.com>
2025-12-04 16:35:02 +0100

    ui fixes

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
 "use client";
+import SpookyGlassButton from "@/components/Button";
 import CardSwap, { Card } from "@/components/CardSwap";
 import CardSwapItem from "@/components/CardSwapItem";
 import Dock from "@/components/Dock";
 import DramaticEmailForm from "@/components/EmailForm";
 import EmailPopup from "@/components/EmailPopup";
+import FloatingBackgroundImages from "@/components/FloatingIcons";
+import FloatingBackgroundIcons from "@/components/FloatingIcons";
+import TargetCursor from "@/components/TargetCursor";
 import { Settings } from "lucide-react";
 import Image from "next/image";
 import { GiMonkey } from "react-icons/gi";
@@ -35,6 +39,12 @@ const items = [
 export default function Home() {
   return (
     <div className="overflow-hidden min-h-screen bg-black relative jungle-ambient particles">
+      <TargetCursor
+        spinDuration={2}
+        hideDefaultCursor={true}
+        parallaxOn={true}
+      />
+      <FloatingBackgroundImages />
       <video
         aria-hidden="true"
         className="absolute inset-0 h-full w-full object-cover opacity-60"
@@ -49,7 +59,7 @@ export default function Home() {
       <div className="absolute inset-0 bg-primary/60 z-10"></div>
 
       <div className="flex flex-col justify-center mt-10">
-        <div className="max-w-[320px] z-40 mx-auto flex justify-center items-center flex-col gap-5">
+        <div className="max-w-[320px] z-40 mx-auto flex justify-center items-center flex-col gap-5 cursor-target ">
           <Image
             src="/logo.png"
             alt="logo"
@@ -71,24 +81,33 @@ export default function Home() {
                hover:shadow-primary transition-all duration-500 overflow-hidden"
         >
           <div className=" max-w-2xl space-y-4 ml-0 md:ml-32">
-            <div className="flex gap-4 justify-center items-center text-2xl font-semibold text-text">
+            <div className="cursor-target flex gap-4 justify-center items-center text-2xl font-semibold text-text">
               <GiMonkey size={55} className="text-text animate-bounce" />
               Enter the Jungle Paradise
             </div>
-
-            <p className="font-eater  leading-relaxed text-green text-xs sm:text-sm md:text-2xl">
-              Get ready for the ultimate jungle beach experience! Dive into
-              vibrant nights filled with pulsating music, tropical cocktails,
-              and playful water games under the stars. Lounge on golden sands,
-              feel the ocean breeze, and immerse yourself in adventure and
-              unforgettable moments. This is where the jungle meets the beach —
-              wild, exhilarating, and completely unforgettable.
-            </p>
+            <div className="">
+              <div className="font-eater cursor-target  leading-relaxed text-green text-xs sm:text-sm md:text-2xl ">
+                Get ready for the ultimate jungle beach experience! Dive into
+                vibrant nights filled with pulsating music, tropical cocktails,
+                and playful water games under the stars. Lounge on golden sands,
+                feel the ocean breeze, and immerse yourself in adventure and
+                unforgettable moments. This is where the jungle meets the beach
+                — wild, exhilarating, and completely unforgettable.
+              </div>
+              <div className="cursor-target ">
+                <SpookyGlassButton
+                  onClick={() => alert("Eerie!")}
+                  className="w-full mt-5"
+                >
+                  Enter
+                </SpookyGlassButton>
+              </div>
+            </div>
           </div>
 
           {/* CARD SECTION */}
           <div>
-            <div className="z-20 relative h-80 md:h-[600px]">
+            <div className="z-20 relative h-80 md:h-[600px] ">
               <CardSwapItem />
             </div>
           </div>
@@ -96,7 +115,7 @@ export default function Home() {
       </div>
       <EmailPopup />
       {/* DOCK */}
-      <div className="relative z-40">
+      <div className="relative z-40 cursor-target ">
         <Dock
           items={items}
           panelHeight={80}
